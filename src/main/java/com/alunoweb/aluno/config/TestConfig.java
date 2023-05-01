352e47a75f89fbc6ada331690b12e15df47be67b
Davi Ferreira da Silva <sildv45@gmail.com>
2023-05-01 17:20:27 -0300

    Payment, one-to-one association

--- src/main/java/com/alunoweb/aluno/config/TestConfig.java
+++ src/main/java/com/alunoweb/aluno/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.alunoweb.aluno.entities.Category;
 import com.alunoweb.aluno.entities.Order;
 import com.alunoweb.aluno.entities.OrderItem;
+import com.alunoweb.aluno.entities.Payment;
 import com.alunoweb.aluno.entities.Product;
 import com.alunoweb.aluno.entities.User;
 import com.alunoweb.aluno.entities.enums.OrderStatus;
@@ -84,5 +85,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
