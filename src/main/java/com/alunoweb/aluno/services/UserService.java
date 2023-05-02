3298c4595b40bc8b07ab165e4ce3000f793d9729
Davi Ferreira da Silva <sildv45@gmail.com>
2023-05-02 10:02:34 -0300

    Exception handling - findById

--- src/main/java/com/alunoweb/aluno/services/UserService.java
+++ src/main/java/com/alunoweb/aluno/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.alunoweb.aluno.entities.User;
 import com.alunoweb.aluno.repositories.UserRepository;
+import com.alunoweb.aluno.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
